40ab6f21e104d5a3a5562dcab7c13ff299f148f0
minju <71gnos@naver.com>
2024-04-24 19:15:16 +0900

    p

--- B_2439.py
+++ B_2439.py
@@ -1,5 +1,3 @@
-N = int(input())
-for i in range((N-1), 0):
-    print(" " * i)
-    for j in range(1,(N+1)):
-        print(" " * j)
+a = int(input())
+for i in range(1,a+1):
+    print(" "*(a-i) + "*"*i)
