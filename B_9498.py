7a9af13787bd6795511b3364afe1996eb3b9d022
minju <71gnos@naver.com>
2024-05-09 10:30:29 +0900

    p

---
+++ B_9498.py
@@ -0,0 +1,20 @@
+# ~ 90 A, ~ 80  B, ~ 70 C, ~ 60 D, 나머지 F
+
+print('FFFFFFDCBAA'[int(input())//10])
+
+'''
+내가 쓴 코드
+
+score = int(input())
+
+if (score >= 90):
+    print("A")
+elif (score >= 80):
+    print("B")
+elif (score >= 70):
+    print("C")
+elif (score >= 60):
+    print("D")
+else:
+    print("F")    
+'''
