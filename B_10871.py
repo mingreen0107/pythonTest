807c404ef1f24a230e4e4f91ffe2c648581aae5a
minju <71gnos@naver.com>
2024-05-12 17:02:51 +0900

    p

--- B_10871.py
+++ B_10871.py
@@ -1,11 +1,8 @@
 # X보다 작은 수를 입력받은 순서대로 공백으로 구분해 출력
 
-listNum, maxNum = map(int, input().split())
-list = []
+N, X = map(int, input().split())
+numList = list(map(int, input().split()))
 
-for i in range(listNum):
-    list.append(int(input().split()))
-
-for i in list:
-    if (i < maxNum):
-        print(i)
+for i in range(N):
+    if numList[i] < X:
+        print(numList[i], end=" ")
